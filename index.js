a2ab05c3508376119da9934299e385f7e2ecc4e6
Navid Atashkhaneh <natashkhaneh@proforma.com>
2022-07-13 11:12:33 -0400

    wip

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ netlifyIdentity.on('init', () => {
 netlifyIdentity.on('login', () => {
     if (initUser == null) {
         // window.location.replace('blog');
-        window.location.replace("navid-login-test.netlify.app/blog.html");
+        window.location.replace("blog.html");
     }
     netlifyIdentity.close();
   });
